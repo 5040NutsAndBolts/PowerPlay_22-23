9fd818ef4ee07f3d72a0bccb276cf6192789d892
eleanor <eleanorgklein@gmail.com>
2022-09-20 11:15:01 -0400

    Broke mechanism testing into its own class and added working robot-oriented driver code into the teleop and hardware classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -4,27 +4,28 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 //import org.firstinspires.ftc.teamcode.helperclasses.HelperMethods;
 
-@TeleOp(name="TeleopCode",group="Teleop")
+@TeleOp(name = "TeleopCode", group = "Teleop")
 public class Teleop extends LinearOpMode
 {
     @Override
     public void runOpMode() throws InterruptedException
     {
+        //initializes robot object
         Hardware robot = new Hardware(hardwareMap);
-        robot.wheelA.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        robot.wheelB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
 
         while(opModeIsActive())
         {
-            robot.wheelA.setPower(gamepad1.right_trigger);
-            robot.wheelB.setPower(gamepad1.right_trigger);
+            //drive method call
+            robot.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
         }
     }
 }
